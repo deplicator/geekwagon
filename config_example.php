536a9ffe3681407fce343cce5b6319ef4c60cbf4
Deplicator <james@geekwagon.net>
2013-04-03 18:24:21 -0500

    Updated readme and config.php example.

---
+++ config_example.php
@@ -0,0 +1,10 @@
+<?php
+//Rename this file config.php and add your api keys.
+
+$BLOGGER_KEY = 'yours';
+$BLOGGER_ID = 'yours';
+
+$FB_APPID_APIKEY = 'yours';
+$FB_APP_SECRET = 'yours';
+
+$BTCGUILD_APIKEY = 'yours';
