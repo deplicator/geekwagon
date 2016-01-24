75f76669098e0f3d3ce82349f046bb51d4d0b06f
James <james@geekwagon.net>
2016-01-24 17:11:37 -0600

    Still figuring out cmj.js.

---
+++ config.js
@@ -0,0 +1,66 @@
+$(function() {
+
+  CMS.init({
+
+    // Name of your site or location of logo file ,relative to root directory (img/logo.png)
+    siteName: 'James Pryor',
+
+    // Tagline for your site
+    siteTagline: 'personal site',
+
+    // Email address
+    siteEmail: 'james@geekwagon.net',
+
+    // Name
+    siteAuthor: 'James Pryor',
+
+    // Navigation items
+    siteNavItems: [
+      { name: 'Github', href: 'https://github.com/deplicator', newWindow: false},
+      { name: 'About'}
+    ],
+
+    // Posts folder name
+    postsFolder: 'blog',
+
+    // Homepage posts snippet length
+    postSnippetLength: 120,
+
+    // Pages folder name
+          pagesFolder: 'pages',
+
+          // Order of sorting (true for newest to oldest)
+          sortDateOrder: true,
+
+    // Site fade speed
+    fadeSpeed: 300,
+
+    // Site footer text
+    footerText: '<a href="">Why?</a>',
+
+    // Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+    // to Github
+    mode: 'Apache',
+
+    // If Github mode is set, your Github username and repo name. Defaults
+    // to Github pages branch (gh-pages)
+    githubUserSettings: {
+      username: 'yourusername',
+      repo: 'yourrepo'
+    }
+
+  });
+
+  // Markdown settings
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+  });
+
+});
